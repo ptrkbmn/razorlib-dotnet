74fe85dcf6f369996868d73d93d420c4021e773a
ptrkbmn <patrick@pbdev.de>
2022-01-09 07:50:58 +0100

    Tag helpers and globalization - Added a Font Awesome tag helper

---
+++ Globalization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbdev.Razor
+{
+  public struct LanguageInfo
+  {
+    public string Code { get; set; }
+    public string Label { get; set; }
+    public string Flag { get; set; }
+
+    public static readonly IDictionary<string, LanguageInfo> LanguageInfos = new Dictionary<string, LanguageInfo>()
+    {
+      { "de", new LanguageInfo() { Code = "de", Label = "Deutsch (German)", Flag = "de" } },
+      { "en", new LanguageInfo() { Code = "en", Label = "English", Flag = "gb" } },
+    };
+
+    public static LanguageInfo Get(string code) => LanguageInfos[code];
+  }
+}
---
+++ TagHelpers/FontAwesomeTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace pbdev.Razor.TagHelpers
+{
+  [HtmlTargetElement("fa")]
+  public class FontAwesomeTagHelper : TagHelper
+  {
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+      var content = await output.GetChildContentAsync();
+      output.TagName = "i";
+      output.Attributes.SetAttribute("class", String.Format("fas fa-{0} fa-fw", content.GetContent().ToLowerInvariant()));
+      output.Content.SetContent(String.Empty);
+    }
+  }
+
+  [HtmlTargetElement("fa2")]
+  public class FontAwesomeTagHelper2 : TagHelper
+  {
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+      var content = await output.GetChildContentAsync();
+      output.TagName = "i";
+      output.Attributes.SetAttribute("class", String.Format("fas fa-{0}", content.GetContent().ToLowerInvariant()));
+      output.Content.SetContent(String.Empty);
+    }
+  }
+}
